69d40d0eb0ad4c5ad628286c5c88bab1298e1292
Adrienne Smith <agsmith046@gmail.com>
2019-06-07 17:08:51 -0500

    added search and edit functions

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
-const STORE = [
-  {id: cuid(), name: "apples", checked: false},
-  {id: cuid(), name: "oranges", checked: false},
-  {id: cuid(), name: "milk", checked: true},
-  {id: cuid(), name: "bread", checked: false}
-];
-
+const STORE = {
+  items: [
+    {id: cuid(), name: "apples", checked: false},
+    {id: cuid(), name: "oranges", checked: false},
+    {id: cuid(), name: "milk", checked: true},
+    {id: cuid(), name: "bread", checked: false}
+  ],
+  hideCompleted: false,
+  searchText: null,
+};
 
 function generateItemElement(item) {
   return `
@@ -19,6 +22,8 @@ function generateItemElement(item) {
         <button class="shopping-item-delete js-item-delete">
             <span class="button-label">delete</span>
         </button>
+        <button class='shopping-item-edit js-item-edit'>
+          <span class='button-label'>edit</span>
       </div>
     </li>`;
 }
@@ -36,16 +41,36 @@ function generateShoppingItemsString(shoppingList) {
 function renderShoppingList() {
   // render the shopping list in the DOM
   console.log('`renderShoppingList` ran');
-  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+  // set up a copy of the store's items in a local variable that we will reassign to a new
+  // version if any filtering of the list occurs
+  let filteredItems = STORE.items;
+
+  // if the `hideCompleted` property is true, then we want to reassign filteredItems to a version
+  // where ONLY items with a "checked" property of false are included
+  if (STORE.hideCompleted) {
+    filteredItems = filteredItems.filter(item => !item.checked);
+  }
+
+  $('.js-shopping-list-search').val(STORE.searchText);
+
+  if (STORE.searchText) {
+    filteredItems = filteredItems.filter(item => item.name.includes(STORE.searchText));
+  }
+
+  // at this point, all filtering work has been done (or not done, if that's the current settings), so
+  // we send our `filteredItems` into our HTML generation function 
+  const shoppingListItemsString = generateShoppingItemsString(filteredItems);
 
   // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
+
 }
 
 
 function addItemToShoppingList(itemName) {
   console.log(`Adding "${itemName}" to shopping list`);
-  STORE.push({id: cuid(), name: itemName, checked: false});
+  STORE.items.push({id: cuid(), name: itemName, checked: false});
 }
 
 function handleNewItemSubmit() {
@@ -61,7 +86,7 @@ function handleNewItemSubmit() {
 
 function toggleCheckedForListItem(itemId) {
   console.log("Toggling checked property for item with id " + itemId);
-  const item = STORE.find(item => item.id === itemId);
+  const item = STORE.items.find(item => item.id === itemId);
   item.checked = !item.checked;
 }
 
@@ -82,11 +107,90 @@ function handleItemCheckClicked() {
 }
 
 
-function handleDeleteItemClicked() {
-  // this function will be responsible for when users want to delete a shopping list
-  // item
-  console.log('`handleDeleteItemClicked` ran')
+// name says it all. responsible for deleting a list item.
+function deleteListItem(itemId) {
+  console.log(`Deleting item with id  ${itemId} from shopping list`)
+
+  // as with `addItemToShoppingLIst`, this function also has the side effect of
+  // mutating the global STORE value.
+  //
+  // First we find the index of the item with the specified id using the native
+  // Array.prototype.findIndex() method. Then we call `.splice` at the index of 
+  // the list item we want to remove, with a removeCount of 1.
+  const itemIndex = STORE.items.findIndex(item => item.id === itemId);
+  STORE.items.splice(itemIndex, 1);
 }
+
+
+function handleDeleteItemClicked() {
+  // like in `handleItemCheckClicked`, we use event delegation
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
+    // get the index of the item in STORE
+    const itemIndex = getItemIdFromElement(event.currentTarget);
+    // delete the item
+    deleteListItem(itemIndex);
+    // render the updated shopping list
+    renderShoppingList();
+  });
+}
+
+// Toggles the STORE.hideCompleted property
+function toggleHideFilter() {
+  STORE.hideCompleted = !STORE.hideCompleted;
+  console.log('`handleToggleHideFilter` ran');
+}
+
+// Places an event listener on the checkbox for hiding completed items
+function handleToggleHideFilter() {
+  $('.js-hide-completed-toggle').on('click', () => {
+    toggleHideFilter();
+    renderShoppingList();
+  });
+}
+
+
+// type in search term and list will filter to items with term
+function searchItemList(searchText) {
+  STORE.searchText = searchText;
+}
+
+function handleSearchItemList() {
+  // console.log('`searchItemList` ran');
+  $('#js-search-list-form').on('submit', event => {
+    event.preventDefault();
+    const searchText = $('.js-shopping-list-search').val();
+    searchItemList(searchText);
+    renderShoppingList();
+  });
+}
+
+function handleSearchClear() {
+  $('#js-search-clear').on('click', () => {
+    searchItemList('');
+    renderShoppingList();
+  })
+}
+
+
+// edit title of an item
+function editListItem(id, newValue) {
+  console.log(`Editing '${id} in shopping list`);
+  const itemIndex = STORE.items.findIndex(item => item.id === id);
+  STORE.items[itemIndex].name = newValue;
+}
+
+function handleEditListItem() {
+  console.log('`handleEditListItem` ran');
+  $('.js-shopping-list').on('click', '.js-item-edit', event => {
+    const itemId = getItemIdFromElement(event.currentTarget);
+    const newValue = prompt("What's the new name?");
+    console.log(newValue);
+    editListItem(itemId, newValue);
+    renderShoppingList();
+  })
+}
+
+
 
 // this function will be our callback when the page loads. it's responsible for
 // initially rendering the shopping list, and activating our individual functions
@@ -97,6 +201,10 @@ function handleShoppingList() {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
+  handleToggleHideFilter();
+  handleEditListItem();
+  handleSearchItemList();
+  handleSearchClear();
 }
 
 // when the page loads, call `handleShoppingList`
